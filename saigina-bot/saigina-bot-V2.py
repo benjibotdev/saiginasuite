ed0e34c30bbe76ecfa21e9af7ff2bb0cc3caa05a
bsmith135 <92476338+bsmith135@users.noreply.github.com>
2022-06-22 20:45:07 -0400

    no message

--- saigina-bot/saigina-bot-V2.py
+++ saigina-bot/saigina-bot-V2.py
@@ -94,8 +94,6 @@ async def twitterApiConfigLoad():
     # twitter api setup
     twitter_bearer_token = api_key_dict["twitter_bearer_token"]
     twitter_stream_account_ids = dictFromFilename("config/twitter_stream_account_ids")
-    print(twitter_stream_account_ids)
-    return
     twitterStreamRule = ""
     for item in twitter_stream_account_ids.keys():
         twitterStreamRule += "from:" + item + " "
